3c813d693ccb7c6c50694feb9425cbd6fc5e5c4b
Adam Nemeth <an@creygames.com>
2021-08-11 15:40:31 +0200

    testing

---
+++ src/Congrats.test.js
@@ -0,0 +1,32 @@
+import { shallow } from "enzyme";
+
+import { findByTestAttr } from "./test/testUtils";
+import Congrats from "./Congrats";
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.length).toBe(1);
+});
+
+test("renders no text when 'success' prop is false", () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
+
+test("render non-empty congrats message whne `success` prop is true", () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
